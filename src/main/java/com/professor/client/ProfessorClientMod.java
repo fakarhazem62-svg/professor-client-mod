488b6f86c877decfb0d070e49344d309409b1e8b
fakarhazem62-svg <fakarhazem62@gmail.com>
2026-06-23 03:25:30 +0300

    Add src/main/java/com/professor/client/ProfessorClientMod.java

---
+++ src/main/java/com/professor/client/ProfessorClientMod.java
@@ -0,0 +1,32 @@
+package com.professor.client;
+
+import com.professor.client.gui.ProfessorScreen;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class ProfessorClientMod implements ClientModInitializer {
+
+    public static KeyBinding openGuiKey;
+
+    @Override
+    public void onInitializeClient() {
+        openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.professorclient.open_gui",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_M,
+                "category.professorclient.general"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openGuiKey.wasPressed()) {
+                if (client.player != null) {
+                    client.setScreen(new ProfessorScreen());
+                }
+            }
+        });
+    }
+}
