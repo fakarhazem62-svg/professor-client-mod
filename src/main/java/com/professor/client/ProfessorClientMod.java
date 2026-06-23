7f1757259e6ebd752a7f11586aef819ec28bc48c
fakarhazem62-svg <fakarhazem62@gmail.com>
2026-06-23 19:03:47 +0300

    Open activation screen on key press

--- src/main/java/com/professor/client/ProfessorClientMod.java
+++ src/main/java/com/professor/client/ProfessorClientMod.java
@@ -1,15 +1,13 @@
 package com.professor.client;
 
-import com.professor.client.gui.ProfessorScreen;
+import com.professor.client.gui.KeyActivationScreen;
 import com.professor.client.gui.ProfessorSplashScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.sound.SoundEvents;
 import org.lwjgl.glfw.GLFW;
 
 public class ProfessorClientMod implements ClientModInitializer {
@@ -23,21 +21,19 @@ public class ProfessorClientMod implements ClientModInitializer {
             client.execute(() -> client.setScreen(new ProfessorSplashScreen()));
         });
 
+        // Keybinding: default P key (changed from M to avoid conflicts)
         openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.professorclient.open_gui",
                 InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_M,
+                GLFW.GLFW_KEY_P,
                 "category.professorclient.general"
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openGuiKey.wasPressed()) {
                 if (client.player != null) {
-                    // Play hype music (Pigstep disc)
-                    client.getSoundManager().play(
-                            PositionedSoundInstance.master(SoundEvents.MUSIC_DISC_PIGSTEP, 1.0f)
-                    );
-                    client.setScreen(new ProfessorScreen());
+                    // Show the activation/loading screen first
+                    client.setScreen(new KeyActivationScreen());
                 }
             }
         });
