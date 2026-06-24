fedbd528f3366303a85bc56aa89aeb51a23f6e7c
fakarhazem62-svg <fakarhazem62@gmail.com>
2026-06-24 04:34:52 +0300

    Add VERSION and CLIENT_NAME constants to fix compile errors

--- src/main/java/com/professor/client/ProfessorClientMod.java
+++ src/main/java/com/professor/client/ProfessorClientMod.java
@@ -1,37 +1,41 @@
 package com.professor.client;
 
-import com.professor.client.gui.ProfessorScreen;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.sound.PositionedSoundInstance;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.sound.SoundEvents;
-import org.lwjgl.glfw.GLFW;
+  import com.professor.client.gui.ProfessorScreen;
+  import net.fabricmc.api.ClientModInitializer;
+  import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+  import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+  import net.minecraft.client.option.KeyBinding;
+  import net.minecraft.client.sound.PositionedSoundInstance;
+  import net.minecraft.client.util.InputUtil;
+  import net.minecraft.sound.SoundEvents;
+  import org.lwjgl.glfw.GLFW;
 
-public class ProfessorClientMod implements ClientModInitializer {
+  public class ProfessorClientMod implements ClientModInitializer {
 
-    public static KeyBinding openGuiKey;
+      public static final String VERSION = "4.0";
+      public static final String CLIENT_NAME = "Professor Client";
 
-    @Override
-    public void onInitializeClient() {
-        openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.professorclient.open_gui",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_M,
-                "category.professorclient.general"
-        ));
+      public static KeyBinding openGuiKey;
 
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (openGuiKey.wasPressed()) {
-                if (client.player != null) {
-                    client.getSoundManager().play(
-                            PositionedSoundInstance.master(SoundEvents.MUSIC_DISC_PIGSTEP, 1.0f)
-                    );
-                    client.setScreen(new ProfessorScreen());
-                }
-            }
-        });
-    }
-}
+      @Override
+      public void onInitializeClient() {
+          openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                  "key.professorclient.open_gui",
+                  InputUtil.Type.KEYSYM,
+                  GLFW.GLFW_KEY_M,
+                  "category.professorclient.general"
+          ));
+
+          ClientTickEvents.END_CLIENT_TICK.register(client -> {
+              while (openGuiKey.wasPressed()) {
+                  if (client.player != null) {
+                      client.getSoundManager().play(
+                              PositionedSoundInstance.master(SoundEvents.MUSIC_DISC_PIGSTEP, 1.0f)
+                      );
+                      client.setScreen(new ProfessorScreen());
+                  }
+              }
+          });
+      }
+  }
+  
